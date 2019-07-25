0784528634532691178ea6b0b229a3a287bfedf7
Eric Conlon <econlon@lyft.com>
2019-07-24 19:21:46 -0700

    bind

--- src/SimplyTyped/Dep.hs
+++ src/SimplyTyped/Dep.hs
@@ -1,30 +1,40 @@
 module SimplyTyped.Dep where
 
+import Data.Text (Text)
 import SimplyTyped.Prelude
+import SimplyTyped.Sub
+
+type Identifier = Text
 
 data UnitExp = UnitExp deriving (Eq, Show)
 
 data UnitTy = UnitTy deriving (Eq, Show)
 
-data ProdExp = ProdExp Exp Exp deriving (Eq, Show)
+data ProdExp a = ProdExp a a deriving (Eq, Show)
 
-data ProdTy = ProdTy Exp Exp deriving (Eq, Show)
+data ProdTy a = ProdTy a a deriving (Eq, Show)
 
-data Exp =
+data BindCon = BindConPi deriving (Eq, Show)
+
+data Bind a = Bind { bindCon :: BindCon, bindType :: a } deriving (Eq, Show)
+
+data Exp a =
       ExpUnit UnitExp
     | ExpUnitTy UnitTy
-    | ExpProd ProdExp
-    | ExpProdTy ProdTy
+    | ExpProd (ProdExp a)
+    | ExpProdTy (ProdTy a)
     deriving (Eq, Show)
 
-data EqualJudge = EqualJudge Exp Exp Exp deriving (Eq, Show)
+newtype ExpScope = ExpScope (Scope (Bind ExpScope) Exp Identifier) deriving (Eq, Show)
 
-data OfTyJudge = OfTyJudge Exp Exp deriving (Eq, Show)
+data EqualJudge a = EqualJudge a a a deriving (Eq, Show)
 
-data IsTyJudge = IsTyJudge Exp deriving (Eq, Show)
+data OfTyJudge a = OfTyJudge a a deriving (Eq, Show)
 
-data Judgement =
-      JudgeEqual EqualJudge
-    | JudgeOfTy OfTyJudge
-    | JudgeIsTy IsTyJudge
+data IsTyJudge a = IsTyJudge a deriving (Eq, Show)
+
+data Judgement a =
+      JudgeEqual (EqualJudge a)
+    | JudgeOfTy (OfTyJudge a)
+    | JudgeIsTy (IsTyJudge a)
     deriving (Eq, Show)
