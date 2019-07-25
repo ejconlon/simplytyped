c9b00175aaf836f43164d795496f75c58edf5a5d
Eric Conlon <econlon@lyft.com>
2019-07-24 18:16:59 -0700

    import tests

---
+++ test/Test/SimplyTyped/Assertions.hs
@@ -0,0 +1,23 @@
+module Test.SimplyTyped.Assertions where
+
+import Control.Monad       (unless)
+import SimplyTyped.Prelude
+import Test.Tasty.HUnit
+
+assertPredicate
+    :: (Show a, HasCallStack)
+    => (a -> a -> Bool)
+    -> String
+    -> a
+    -> a
+    -> Assertion
+assertPredicate predicate preface left right =
+    unless (predicate left right) (assertFailure msg) where
+        msg = (if null preface then "" else preface ++ "\n") ++
+            "left: " ++ show left ++ "\n right: " ++ show right
+
+assertNotEqual :: (Eq a, Show a, HasCallStack) => a -> a -> Assertion
+assertNotEqual = assertPredicate (/=) "Expected not equal"
+
+(@/=) :: (Eq a, Show a, HasCallStack) => a -> a -> Assertion
+(@/=) = assertNotEqual
