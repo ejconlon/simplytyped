8544923cc8b6e72e1f12f80014f0548daba9e107
Eric Conlon <econlon@lyft.com>
2019-07-27 21:12:56 -0700

    stub repl

--- src/SimplyTyped/Main.hs
+++ src/SimplyTyped/Main.hs
@@ -1,6 +1,25 @@
 module SimplyTyped.Main where
 
+import qualified Data.Map.Strict as Map
+import SimplyTyped.Cli
+import SimplyTyped.NiceRepl
 import SimplyTyped.Prelude
 
+type ReplState = ()
+
+initReplState :: ReplState
+initReplState = ()
+
+optCommands :: OptionCommands ReplState
+optCommands = Map.empty
+
+execCommand :: Command ReplState
+execCommand = const (pure ReplContinue)
+
+replDef :: ReplDef ReplState
+replDef =
+  ReplDef
+    {rdGreeting = "hello", rdInitState = initReplState, rdAdditionalCommands = optCommands, rdExecCommand = execCommand}
+
 exe :: IO ()
-exe = putStrLn "todo"
+exe = runRepl replDef
